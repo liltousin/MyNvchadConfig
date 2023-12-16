a5c3c616cf808e12001734c647d2513919b0942d
liltousin <sergeypletnev2004@gmail.com>
2023-12-16 22:07:14 +0300

    init commit

---
+++ custom/configs/null-ls.lua
@@ -0,0 +1,53 @@
+local null_ls = require "null-ls"
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local b = null_ls.builtins
+
+local sources = {
+
+  -- webdev stuff
+  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+
+  -- Lua
+  b.formatting.stylua,
+
+  -- cpp
+  b.formatting.clang_format,
+
+  b.formatting.gofumpt,
+  b.formatting.goimports_reviser,
+  b.formatting.golines,
+
+  b.diagnostics.mypy.with {
+    extra_args = function()
+      local virtual = os.getenv "VIRTUAL_ENV" or os.getenv "CONDA_PREFIX" or "/usr"
+      return { "--python-executable", virtual .. "/bin/python3" }
+    end,
+  },
+  b.diagnostics.ruff,
+  b.formatting.black,
+  b.formatting.isort,
+}
+
+local on_attach = function(client, bufnr)
+  if client.supports_method "textDocument/formatting" then
+    vim.api.nvim_clear_autocmds {
+      group = augroup,
+      buffer = bufnr,
+    }
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format { bufnr = bufnr }
+      end,
+    })
+  end
+end
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+  on_attach = on_attach,
+}
---
+++ custom/mappings.lua
@@ -0,0 +1,109 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  },
+  v = {
+    [">"] = { ">gv", "indent" },
+  },
+}
+
+-- more keybinds!
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dus"] = {
+      function()
+        local widgets = require "dap.ui.widgets"
+        local sidebar = widgets.sidebar(widgets.scopes)
+        sidebar.open()
+      end,
+      "Open debugging sidebar",
+    },
+    ["<F5>"] = {
+      function()
+        require("dap").continue()
+      end,
+      "Continue debugging",
+    },
+    ["<F10>"] = {
+      function()
+        require("dap").step_over()
+      end,
+      "Step over",
+    },
+    ["<F11>"] = {
+      function()
+        require("dap").step_into()
+      end,
+      "Step into",
+    },
+    ["<F12>"] = {
+      function()
+        require("dap").step_out()
+      end,
+      "Step out",
+    },
+    ["<F6>"] = {
+      "<cmd> DapTerminate <CR>",
+      "Add breakpoint at line",
+    },
+  },
+}
+
+M.dap_go = {
+  plugin = true,
+  n = {
+    ["<leader>dgt"] = {
+      function()
+        require("dap-go").debug_test()
+      end,
+      "Debug go test",
+    },
+    ["<leader>dgl"] = {
+      function()
+        require("dap-go").debug_last()
+      end,
+      "Debug last go test",
+    },
+  },
+}
+
+M.gopher = {
+  plugin = true,
+  n = {
+    ["<leader>gsj"] = {
+      "<cmd> GoTagAdd json <CR>",
+      "Add json stuct tags",
+    },
+    ["<leader>gsy"] = {
+      "<cmd> GoTagAdd yaml <CR>",
+      "Add yaml stuct tags",
+    },
+    ["<leader>gie"] = {
+      "<cmd> GoIfErr <CR>",
+      "Add if err block",
+    },
+  },
+}
+
+M.dap_python = {
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function()
+        require("dap-python").test_method()
+      end,
+      "Debug python run",
+    },
+  },
+}
+
+return M
